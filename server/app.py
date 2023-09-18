c5e3b43ecaf60ed756b8f11d2d73fc4698a0659f
Franklin Hau <fhauchi@gmail.com>
2023-09-18 11:49:56 -0600

    Password Protection Lab

--- server/app.py
+++ server/app.py
@@ -9,7 +9,6 @@ from models import User
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
@@ -18,24 +17,46 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        json_data = request.get_json()
+        user = User(username=json_data['username'])
+        user.password_hash = json_data['password']
+
         db.session.add(user)
         db.session.commit()
-        return user.to_dict(), 201
+        return {"id": user.id, "username": user.username}, 201
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return {"id": user.id, "username": user.username}, 200
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        user = User.query.filter(User.username == json_data['username']).first()
+
+        if user and user.authenticate(json_data['password']):
+            session['user_id'] = user.id 
+            return {"id": user.id, "username": user.username}, 200
+        
+        return {'message': 'Invalid credentials'}, 401
+        
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
+    
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')    
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
